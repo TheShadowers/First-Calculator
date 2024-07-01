91a95d9f7a5340de25c056c94b622aec81f7100f
Sherwin <sherwinliusherwinliu@gmail.com>
2024-07-01 22:37:28 +0100

    First commit with all project files

---
+++ autosave/temp.py
@@ -0,0 +1,31 @@
+
+
+def is_valid(expression):
+    valid_chars = "0123456789+-*/() "
+    for char in expression:
+        if char not in valid_chars:
+            return False
+    return True
+    
+    
+while True:
+    calc_op = input("What do you want to calculate?\n")
+    if is_valid(calc_op):  
+        if "+" in calc_op:
+            operands = calc_op.split("+")
+            print(float(operands[0])+float(operands[1]))
+        elif "-" in calc_op:
+            operands = calc_op.split("-")
+            print(float(operands[0])-float(operands[1]))
+        elif "/" in calc_op:
+            operands = calc_op.split("/")
+            print(float(operands[0])/float(operands[1]))
+        elif '*' in calc_op:
+            operands = calc_op.split("*")
+            print(float(operands[0])*float(operands[1]))          
+    else:
+        print("Invalid input, try again.")
+    
+    
+    
+ 
---
+++ history.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Jul  1 01:25:00 2024)---
+runcell(0, 'C:/Users/user/.spyder-py3/temp.py')
+debugfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+runcell(0, 'C:/Users/user/.spyder-py3/temp.py')
+debugfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+runfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+debugfile('C:/Users/user/.spyder-py3/temp.py', wdir='C:/Users/user/.spyder-py3')
+runcell(0, 'C:/Users/user/.spyder-py3/temp.py')
+(34+2)*2
+runcell(0, 'C:/Users/user/.spyder-py3/temp.py')
+runcell(0, 'C:/Users/user/.spyder-py3/calculator.py')
